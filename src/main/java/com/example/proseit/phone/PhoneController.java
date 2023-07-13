be32bbecf8f271f9b6abaa14d465237b35d3f9a4
jamil-stark <kunoomaking@gmail.com>
2023-07-13 18:19:23 +0300

    #Create PhoneController class The @RestController annotation is moved from ProseitApplication.java to the controller. The @GetMapping annotation plus the following method are also moved. A @RequestMapping annotation that has a path is also used at top of the class. The method hello() is renamed to getPhones().

---
+++ src/main/java/com/example/proseit/phone/PhoneController.java
@@ -0,0 +1,28 @@
+package com.example.proseit.phone;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/phone")
+public class PhoneController {
+
+    @GetMapping
+    public List<Phone> getPhones() {
+        return List.of(
+                new Phone(
+                        1L,
+                        "Samsung",
+                        "SJSG4909495",
+                        "S10",
+                        LocalDate.of(2017, Month.JUNE, 21),
+                        500000
+                )
+        );
+    }
+}
